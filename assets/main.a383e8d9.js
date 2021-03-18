e6a0c7a6417c544f4b167a8c145e555541a431e4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-18 08:42:07 +0000

    Deploy idl.ist to github.com/idlist/idlist.github.io.git:gh-pages

---
+++ assets/main.a383e8d9.js
@@ -0,0 +1,1 @@
+import"./dynamic-import-polyfill.b3685604.js";import{m as t}from"./vendor.01f21009.js";const e={view:()=>[t("h1","Sample Text"),t("p.sample-text","Sample Text"),t("a",{href:"./test/"},"Go to test")]};t.mount(document.body,e);
