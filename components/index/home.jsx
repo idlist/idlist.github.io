2409a4881f34b246c1b3b3a5ef99fa6f151621f0
i'DLisT <i.dlist@outlook.com>
2021-04-07 17:10:06 +0800

    feat(home): add content

--- components/index/home.jsx
+++ components/index/home.jsx
@@ -5,10 +5,10 @@ import HomeCard from './home-card'
 import './home.sass'
 
 const SocialMediaList = [
-  { site: 'Twitter', account: '@i_dlist', path: '/icons/twitter.png', link: 'https://twitter.com/i_dlist' },
-  { site: 'SoundCloud', account: 'i\'DLisT', path: '/icons/soundcloud.png', link: 'https://soundcloud.com/idlist' },
-  { site: 'GitHub', account: 'idlist', path: '/icons/github.png', link: 'https://github.com/idlist' },
-  { site: 'Stage1st', account: 'i\'DLisT', path: '/icons/stage1st.png', link: 'https://bbs.saraba1st.com/' }
+  { site: 'Twitter', account: '@i_dlist', icon: '/icons/twitter.png', link: 'https://twitter.com/i_dlist' },
+  { site: 'SoundCloud', account: 'i\'DLisT', icon: '/icons/soundcloud.png', link: 'https://soundcloud.com/idlist' },
+  { site: 'GitHub', account: 'idlist', icon: '/icons/github.png', link: 'https://github.com/idlist' },
+  { site: 'Stage1st', account: 'i\'DLisT', icon: '/icons/stage1st.png', link: 'https://bbs.saraba1st.com/' }
 ]
 
 class SocialMedia {
@@ -18,7 +18,7 @@ class SocialMedia {
         <div class='social-media-grid'>
           { SocialMediaList.map(item => (
             <a class='social-media-item' href={ item.link } target='blank'>
-              <img src={ item.path } alt={ item.site } />
+              <img src={ item.icon } alt={ item.site } />
               <span class='social-media-platform'>{ item.site } - </span>
               <span class='social-media-name'>{ item.account }</span>
             </a>
@@ -32,8 +32,19 @@ class SocialMedia {
 const ThingsList = [
   {
     type: 'Coding',
+    class: 'coding',
     sites: [
-      { site: 'Koishi', link: 'https://koishi.js.org/guide/starter.html' }
+      { site: 'Mithril', icon: '/icons/mithril.png', link: 'https://mithril.js.org/' },
+      { site: 'Vite', icon: '/icons/vite.png', link: 'https://cn.vitejs.dev/' },
+      { site: 'Koishi', icon: '/icons/koishi.png', link: 'https://koishi.js.org/' }
+    ]
+  },
+  {
+    type: 'Graphics',
+    class: 'graphics',
+    sites: [
+      { site: 'Photoshop', icon: '/icons/photoshop.png' },
+      { site: 'Illustrator', icon: '/icons/illustrator.png' }
     ]
   }
 ]
@@ -43,12 +54,20 @@ class Things {
     return (
       <HomeCard cardTitle='Things i use...'>
         { ThingsList.map(category => (
-          <>
-            <h2>{ category.type }</h2>
-            { category.sites.map(site => (
-              <a href={ site.link }>{ site.site }</a>
-            )) }
-          </>
+          <div class='things-used'>
+            <h2 class={ 'things-used-title ' + category.class }>{ category.type }</h2>
+            <div class='things-used-list'>
+              { category.sites.map(site => (
+                <div class='things-used-item'>
+                  <img src={ site.icon } alt={ site.site }/>
+                  { site.link
+                    ? <a href={ site.link } target='blank'>{ site.site }</a>
+                    : <span>{ site.site }</span>
+                  }
+                </div>
+              )) }
+            </div>
+          </div>
         )) }
       </HomeCard>
     )
@@ -60,6 +79,7 @@ class Home {
     return (
       <>
         <SocialMedia />
+        <Things />
         <Footer />
       </>
     )
